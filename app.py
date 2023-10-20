fb9a7be084a867161a289e1533257fa75ec84eee
Ricardo <ricardo.oamaro@gmail.com>
2023-10-20 01:06:57 -0300

    melhoria na comparacao das listas

--- app.py
+++ app.py
@@ -16,22 +16,36 @@ spreadsheet = gc.open("bradesco")
 sheet = spreadsheet.worksheet('meses')
 
 get_month = sheet.cell(5, 1).value.capitalize()
-print(get_month)
 
 month_sheet = spreadsheet.worksheet(get_month)
 
 get_data = month_sheet.get_values('A2:C')
-print(get_data)
 
-########################################
+empty_line = month_sheet.col_values(1)
 
-#TODO adicionar os itens uma nova lista e fazer o update nas linhas vazias
+next_line = len(empty_line) + 1
 
 linhas = []
+updated_lines = []
+
+invoice = [
+    ['25/05', 'RAYOBYTE 521177', '79,20'],
+    ['25/01', 'PAG*Box46 -CT 05/05', '87,00'],
+]
 
 
 def update_invoice():
-    month_sheet.update('A2:C', get_data)
+    for i in linhas:
+        for j, value in enumerate(i, start=1):
+            month_sheet.update_cell(next_line, j, value)
+        update_nextline()
+
+
+def update_nextline():
+    global next_line
+    global empty_line
+    empty_line = month_sheet.col_values(1)
+    next_line = len(empty_line) + 1
 
 
 def compare_lists(sheet_list, invoice_list):
@@ -39,26 +53,25 @@ def compare_lists(sheet_list, invoice_list):
 
     for item in invoice_list:
         if tuple(item) not in set_list:
-            get_data.append(item)
+            updated_lines.append(item)
 
 
 if sms_receiver == 'True':
     linhas = result_list
-    empty_line = month_sheet.col_values(1)
-    next_line = len(empty_line) + 1
     month_sheet.update_cell(next_line, 1, linhas[0][0])
     month_sheet.update_cell(next_line, 2, linhas[0][1])
     month_sheet.update_cell(next_line, 3, linhas[0][2])
 elif itau_pdf.fatura_itau:
+    print('entrei no if do itau')
     linhas = itau_pdf.result_list2
-    compare_lists(get_data, itau_pdf.result_list2)
+    compare_lists(get_data, linhas)
     update_invoice()
+    print(updated_lines)
 else:
     linhas = handled_list
     print('executado essa linha')
+    compare_lists(get_data, linhas)
     update_invoice()
-
-for i in get_data:
-    print(i)
+    print(updated_lines)
 
 sys.exit(0)
